175839cf30b1a6186a61350adc345b898e236000
TomasBack2Future <tomas.back2future@gmail.com>
2026-09-26 15:58:43 +0800

    test: isolate CLI tests from the operator ledger (#41)
    * test: isolate all CLI tests from the operator ledger
    
    * test: keep plugin hook fixtures local to each test

--- cmd/squad/main_test.go
+++ cmd/squad/main_test.go
@@ -2,10 +2,31 @@ package main
 
 import (
 	"bytes"
+	"fmt"
+	"os"
 	"regexp"
 	"strings"
 	"testing"
 )
+
+// Even commands such as version run post-command hygiene. Keep the default
+// test process away from the operator's live ledger; individual tests can
+// still replace SQUAD_HOME with their own fixtures through t.Setenv.
+func TestMain(m *testing.M) {
+	home, err := os.MkdirTemp("", "squad-cli-test-home-")
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+	if err := os.Setenv("SQUAD_HOME", home); err != nil {
+		_ = os.RemoveAll(home)
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+	code := m.Run()
+	_ = os.RemoveAll(home)
+	os.Exit(code)
+}
 
 func TestVersionString(t *testing.T) {
 	if versionString == "" {
